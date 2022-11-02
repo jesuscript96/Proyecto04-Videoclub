03984e5ba6c95e23c24bb0d8ff4b713f1dcbfb80
JesúsVCh <jvalenzuela.chulia@gmail.com>
2022-11-02 23:57:23 +0100

    crud films done

--- controllers/FilmControllers.js
+++ controllers/FilmControllers.js
@@ -1,10 +1,13 @@
 const Film = require('../models/film')
 
+const db = require("../db/db")
+
 const FilmController = {}
 
 FilmController.getAllFilms = async (req, res) => {
     try {
-        let resp = await Film.findAll()
+        let resp = await Film.findAll({
+        })
             .then(resp => {
                 res.send(resp)
             })
@@ -13,5 +16,60 @@ FilmController.getAllFilms = async (req, res) => {
     }
 }
 
+FilmController.getTopFilms = async (req, res) => {
+    try {
+        let resp = await Film.findAll({
+            order: [["rating", "DESC"]],
+            limit: 5
+        })
+            .then(resp => {
+                res.send(resp)
+            })
+    } catch (err) {
+        res.send(err)
+    }
+}
+
+FilmController.getFilmById = async (req, res) => {
+    try {
+        let id = req.params.id
+        let resp = await Film.findOne({
+            where: {id_film: id}
+        })
+            .then(resp => {
+                res.send(resp)
+            })
+    } catch (err) {
+        res.send(err)
+    }
+}
+
+FilmController.getFilmByTitle = async (req, res) => {
+    try {
+        let title = req.params.title
+        let resp = await Film.findOne({
+            where: {title: title}
+        })
+            .then(resp => {
+                res.send(resp)
+            })
+    } catch (err) {
+        res.send(err)
+    }
+}
+
+FilmController.getFilmByGenre = async (req, res) => {
+    try {
+        let genre = req.params.genre
+        let resp = await Film.findAll({
+            where: {genre: genre}
+        })
+            .then(resp => {
+                res.send(resp)
+            })
+    } catch (err) {
+        res.send(err)
+    }
+}
 
 module.exports = FilmController
