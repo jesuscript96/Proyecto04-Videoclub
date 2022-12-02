d599a360e08462a80d1e49940d132082ea105a56
JesúsVCh <jvalenzuela.chulia@gmail.com>
2022-12-02 18:34:02 +0100

    deploying

--- db/db.js
+++ db/db.js
@@ -4,12 +4,12 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize (
     process.env.MYSQL_DATABASE,
-    process.env.MYSQL_USER,
+    process.env.MYSQL_USERNAME,
     process.env.MYSQL_PASSWORD,
     { 
       host: process.env.MYSQL_HOST,
       port: process.env.MYSQL_PORT,
-      dialect: process.env.DIALECT 
+      dialect: process.env.MYSQL_DIALECT
     }
 )
 
